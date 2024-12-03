4f569fe701250624b230375ff56503e9671ae7f5
Manoj Gövert <mg@macbookpro.localdomain>
2024-12-03 10:36:19 +0100

    Add GitHub Pages configuration to Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
 export default defineConfig({
+  site: 'https://ManojGvrt.github.io',
+  base: '/rims',
   integrations: [
     tailwind({
       applyBaseStyles: false,
